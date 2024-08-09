f5d958235f43860ba80857739ec56748a7f4a1b4
VenkateshKatta1 <venkateshkatta5@gmail.com>
2024-08-09 12:39:09 -0500

    Backend

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotEnv = require("dotenv");
+const mongoose = require("mongoose");
+const vendorRoutes = require("./routes/vendorRoutes");
+const bodyParser = require("body-parser");
+const firmRoutes = require("./routes/firmRoutes");
+const productRoutes = require("./routes/productRoutes");
+const path = require("path");
+
+const app = express();
+
+dotEnv.config();
+const PORT = 8080;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((e) => console.log(e));
+
+app.use(bodyParser.json());
+app.use("/vendor", vendorRoutes);
+app.use("/firm", firmRoutes);
+app.use("/product", productRoutes);
+app.use("/uploads", express.static("uploads"));
+
+app.listen(PORT, () => {
+  console.log(`Server started and running at ${PORT}`);
+});
+
+app.use("/home", (req, res) => {
+  res.send("<h1>Welcome to SUBY</h1>");
+});
