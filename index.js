b59ffe8fa125153ef6cd6c8b91da7060d0bf5492
VenkateshKatta1 <venkateshkatta5@gmail.com>
2024-08-12 15:37:00 -0500

    Added path module

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const vendorRoutes = require("./routes/vendorRoutes");
 const bodyParser = require("body-parser");
 const firmRoutes = require("./routes/firmRoutes");
 const productRoutes = require("./routes/productRoutes");
-const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 dotEnv.config();
 const PORT = process.env.PORT || 8080;
